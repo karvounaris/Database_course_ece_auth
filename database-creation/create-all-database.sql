614c76651136b77abd02463e7a48f0be50591377
deepblue597 <iasonas2001@hotmail.com>
2023-12-16 13:17:06 +0200

    FIXED DISASTERSITUATION PROBLEM

--- database-creation/create-all-database.sql
+++ database-creation/create-all-database.sql
@@ -176,30 +176,31 @@ VALUES
 -- Create the disaster_situation table with the foreign key
 CREATE TABLE disaster_situation (
     RGDP INT,
-    disasterSituationID INT,
+    disasterSituationID  INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     financial_aid DECIMAL(10, 2),
     recovery_time INT,
     Total_cost DECIMAL(10, 2),
     Plan_title VARCHAR(255),
     country_name VARCHAR(255),
-    PRIMARY KEY (disasterSituationID, country_name),
-    FOREIGN KEY (country_name) REFERENCES Country(country_name),
-    FOREIGN KEY (disasterSituationID) REFERENCES Disaster(Disaster_ID)
+    disaster_id INT,
+    FOREIGN KEY (country_name) REFERENCES Country(country_name), 
+    FOREIGN KEY (disaster_id) REFERENCES Disaster(Disaster_ID)
 );
 
+
 -- Insert 10 rows into the disaster_situation table with the foreign key
-INSERT INTO disaster_situation (RGDP, financial_aid, recovery_time, Total_cost, Plan_title, country_name)
+INSERT INTO disaster_situation (RGDP, financial_aid, recovery_time, Total_cost, Plan_title, country_name, disaster_id)
 VALUES
-(1, 5000.00, 30, 100000.00, 'Recovery Plan A', 'CountryA'),
-(2, 10000.00, 45, 150000.00, 'Recovery Plan B', 'CountryB'),
-(3, 7500.00, 25, 120000.00, 'Recovery Plan C', 'CountryC'),
-(4, 12000.00, 40, 180000.00, 'Recovery Plan D', 'CountryD'),
-(5, 8000.00, 35, 130000.00, 'Recovery Plan E', 'CountryE'),
-(6, 15000.00, 50, 200000.00, 'Recovery Plan F', 'CountryF'),
-(7, 9000.00, 28, 140000.00, 'Recovery Plan G', 'CountryG'),
-(8, 11000.00, 42, 160000.00, 'Recovery Plan H', 'CountryH'),
-(9, 9500.00, 32, 170000.00, 'Recovery Plan I', 'CountryI'),
-(10, 13000.00, 48, 220000.00, 'Recovery Plan J', 'CountryJ');
+(1, 5000.00, 30, 100000.00, 'Recovery Plan A', 'CountryA', 1),
+(2, 10000.00, 45, 150000.00, 'Recovery Plan B', 'CountryB', 2),
+(3, 7500.00, 25, 120000.00, 'Recovery Plan C', 'CountryC', 3),
+(4, 12000.00, 40, 180000.00, 'Recovery Plan D', 'CountryD', 4),
+(5, 8000.00, 35, 130000.00, 'Recovery Plan E', 'CountryE', 5),
+(6, 15000.00, 50, 200000.00, 'Recovery Plan F', 'CountryF', 6),
+(7, 9000.00, 28, 140000.00, 'Recovery Plan G', 'CountryG', 7),
+(8, 11000.00, 42, 160000.00, 'Recovery Plan H', 'CountryH', 8),
+(9, 9500.00, 32, 170000.00, 'Recovery Plan I', 'CountryI', 9),
+(10, 13000.00, 48, 220000.00, 'Recovery Plan J', 'CountryJ', 10);
 
 
 
